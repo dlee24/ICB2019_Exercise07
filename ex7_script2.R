58b32f2d82ed6895b758a57b05c481af836ccc0a
Diane Lee <dlee24@nd.edu>
2019-11-13 12:09:33 -0500

    New notes

--- ex7_script2.R
+++ ex7_script2.R
@@ -15,13 +15,14 @@ NumObs <- function(Species){
 #part two: function to return a df for flowers with Sepal.Width > specified value by user.
 
 #usage: SepalWidthMinimum(minimum_width_desired,dataset_name)
-#defaults to iris as dataset, will work for other dataframes with Septal.Width column
+#defaults to iris as dataset, will work for other dataframes with Sepal.Width column
 SepalWidthMin <- function(minWidth, file=iris){
   df = file[file$Sepal.Width > minWidth,]
   return(df)
 }
 
-
+#csv file for given species
+#usage SpeciesWriter("speciesName")
 SpeciesWriter <- function(SpeciesName){
   SpeciesFile = iris[iris$Species == SpeciesName,]
   write.csv(SpeciesFile, file = paste(SpeciesName,".csv", sep = ""))
